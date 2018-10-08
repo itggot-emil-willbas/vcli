c2d5be66e2af47cfc6896a6d0133ef798e110e7d
Emil Willbas <emil@vildbas.com>
2018-10-08 14:39:46 +0200

    1.2 SFC
    Nice! Tog ett projekt med vue-router, tog bort router-kopplingen. Lade till tre egna components och fick dem att fungera!

---
+++ vue-cli-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <router-view/> -->
+    <HelloWorld></HelloWorld>
+    <EmilHeader></EmilHeader>
+    <EmilMain></EmilMain>
+    <EmilFooter></EmilFooter>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import EmilHeader from './components/EmilHeader.vue'
+import EmilMain from './components/EmilMain.vue'
+import EmilFooter from './components/EmilFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    EmilHeader,
+    EmilMain,
+    EmilFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-project/src/components/EmilFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmilFooter',
+  data () {
+    return {
+      msg: 'Welcome Emil W to the Footer'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color:gray;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
